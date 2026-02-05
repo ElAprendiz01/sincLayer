267679687454d2da85f3edf2a84e09b1ce3f5489
El_aprendiz <Gmaycol963@gmail.com>
2026-02-04 21:39:48 -0600

    Agregar archivos de proyecto.

---
+++ application/Interfaces/IEstdoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application.Interfaces
+{
+    public interface IEstdoRepositorio
+    {
+        Task<IEnumerable<Estado_domain>> ListarEstadosAsync();
+        Task<IEnumerable<Estado_domain>> ListarEstadospornombreAsync();
+        Task NuevoEstado<estado_domain>(Estado_domain oestado);
+        Task ActualizarEstado<estado_domain>(Estado_domain oestado);
+        Task EliminarEstado<estado_domain>(int idestado);
+
+
+
+    }
+}
---
+++ infrastructure/DB/DBconexionfactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace infrastructure.DB
+{
+    public class DBconexionfactory
+    {
+        private readonly string _connectionString;
+
+        public DBconexionfactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
