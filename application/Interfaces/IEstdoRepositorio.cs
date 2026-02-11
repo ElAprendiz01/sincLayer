94cb2670a35253bd02d4501c8da11eb008a2f075
El_aprendiz <Gmaycol963@gmail.com>
2026-02-10 19:53:57 -0600

    estados pendienteshasta interfaces

--- application/Interfaces/IEstdoRepositorio.cs
+++ application/Interfaces/IEstdoRepositorio.cs
@@ -9,13 +9,13 @@ namespace application.Interfaces
 {
     public interface IEstdoRepositorio
     {
-        Task<IEnumerable<Estado_domain>> ListarEstadosAsync();
+        Task<IEnumerable<Estado_domain>> ListarEstadosAsync(); // viende del archivo domain, es el nombre de la clase que se creo en el domain
         Task<IEnumerable<Estado_domain>> ListarEstadospornombreAsync();
         Task NuevoEstado<estado_domain>(Estado_domain oestado);
         Task ActualizarEstado<estado_domain>(Estado_domain oestado);
         Task EliminarEstado<estado_domain>(int idestado);
         // ver
-
+        // las
 
 
     }
