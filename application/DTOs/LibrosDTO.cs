6138625b7a0aafc301957815be45cad0a4e4b924
rebeca_artola <rebeca.artola07@gmail.com>
2026-03-02 18:09:00 -0600

    dto

---
+++ application/DTOs/LibrosDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace application.DTOs
+{
+    public class LibrosDTO
+    {
+        
+            public int? Id_Libro { get; set; }
+            public string? Titulo { get; set; }
+            public string? ISBN { get; set; }
+            public int? Id_Autor { get; set; }
+            public string? Nombre_Autor { get; set; } 
+            public int? Id_Categoria { get; set; }
+            public string? Nombre_Categoria { get; set; } 
+            public string? Editorial { get; set; }
+            public int? Año_Publicacion { get; set; }
+            public int? Stock { get; set; }
+            public DateTime Fecha_Creacion { get; set; }
+            public DateTime? Fecha_Modificacion { get; set; }
+            public int? Id_Creador { get; set; }
+            public int? Id_Modificador { get; set; }
+            public int? Id_Estado { get; set; }
+            public string? Estado { get; set; }
+            public bool? ForzarRecuperacion { get; set; }
+
+
+    }
+}
---
+++ application/Interfaces/ILibroRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application.Interfaces
+{
+    public class ILibroRepository
+    {
+        public interface ILibrosRepository
+        {
+            Task<IEnumerable<LibroDomain>> Listar_LibrosAsync();
+            Task<IEnumerable<LibroDomain>> Listar_Libros_Por_AutorAsync(int idAutor);
+            Task<IEnumerable<LibroDomain>> Listar_Libros_Por_CategoriaAsync(int idCategoria);
+            Task NuevoLibroAsync(LibroDomain libro);
+            Task EditarLibroAsync(LibroDomain libro);
+            Task EliminarLibroAsync(int idLibro, int idModificador);
+        }
+
+    }
+}
