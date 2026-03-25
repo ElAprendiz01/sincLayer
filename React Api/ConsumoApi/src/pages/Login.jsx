c768d972ce2cc95b9c79ee4e7f7896549380b5ec
ElAprendiz01 <gmaycol963@gmail.com>
2026-03-24 23:49:41 -0600

    cambio de endopoit

--- React Api/ConsumoApi/src/pages/Login.jsx
+++ React Api/ConsumoApi/src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
     }
 
 try {
-      const response = await fetch("http://localhost:5082/api/Usuario/login", {
+      const response = await fetch("https://localhost:44305/api/Usuario/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
